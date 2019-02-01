457963bf0490bc35ec354e57d4633da0db16320f
Kyle Ngo <Quichey@Kyles-MacBook-Pro-2.local>
2019-01-31 17:48:23 -0800

    test

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,70 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  total = 0
+
+  arr.each do |n|
+    total += n
+  end
+
+  return total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return sum(arr.sort.reverse[0, 2])
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  for first in 0...arr.length
+    for second in (first+1)...arr.length
+      return true if (arr[first] + arr[second] == n)
+    end
+  end
+
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false unless s[0].to_s =~ /[[:alpha:]]/
+
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  not vowels.include?(s.downcase[0])
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if s =~ /[^0-1 ]/
+
+  last_char = s[s.length - 1]
+  second_to_last_char = s[s.length - 2]
+  third_to_last_char = s[s.length - 3]
+
+  second_to_last_bit = second_to_last_char != " " ? second_to_last_char : third_to_last_char
+  return (last_char == "0") && (second_to_last_bit == "0")
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def initialize(isbn, price)
+    if isbn == "" || price <= 0
+      raise ArgumentError.new("ISBN is empty string or Price is less than or equal to 0.")
+    end
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string()
+    "$#{'%.2f' % @price}"
+  end
 end
